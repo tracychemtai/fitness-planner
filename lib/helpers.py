d8c5f30506ea44511b213f03ad746074affcb802
CHARITY-WACHIRA <kouwywachira@gmail.com>
2024-06-10 13:42:31 +0300

    first implentation of crude function

--- lib/helpers.py
+++ lib/helpers.py
@@ -3,3 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from prettycli import bold
 from simple_term_menu import TerminalMenu
 import os
+
+#initial configuration for SQLAlchemy
+def get_session(engine):
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+# data base operation function
+def create_workout(session, name):
+    workout = Workout(name=name)
+    session.add(workout)
+    session.commit()
+    return workout

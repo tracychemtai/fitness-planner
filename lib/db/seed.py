8fd701124d10ed829e32fd3105c014452d825c34
ngotwaaron <ngotwaaron@gmail.com>
2024-06-11 16:18:28 +0300

    Initialized database session with SQLAlchemy

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -3,3 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 
 print("🌱 Seeding DB...")
+
+engine = create_engine('sqlite:///fitness_data.db')
+Session = sessionmaker(bind=engine)
+session = Session()
